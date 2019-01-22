ae2c04e0bda6fff5f7153cc35990b40f46a3ae29
aesosa <Andres.Sosa@imail2.org>
2019-01-22 12:52:32 -0700

    Working on creating a stable build.

--- NVL9.Samples.SSApiTest1/NVL9.Samples.SSApiTest1.ServiceModel/Hello.cs
+++ NVL9.Samples.SSApiTest1/NVL9.Samples.SSApiTest1.ServiceModel/Hello.cs
@@ -1,13 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using ServiceStack;
+﻿// <copyright file="Hello.cs" company="SelectHealth.org">
+//     Copyright (c) 2019 SelectHealth.org.  All rights reserved.
+// </copyright>
+// <summary>
+//     Document created on 2019-01-22-9:11 AM
+// </summary>
+// <author>Andres Sosa (imail2)</author>
 
 namespace NVL9.Samples.SSApiTest1.ServiceModel
 {
+    using ServiceStack;
+
     [Route("/hello/{Name}")]
-    public class Hello : IReturn<HelloResponse>
+    public class Hello : IPost, IReturn<HelloResponse>
     {
         public string Name { get; set; }
     }
