ea4dd07c29018099edf6ef7f8f635d5b282f74db
aesosa <Andres.Sosa@imail2.org>
2019-01-23 16:26:07 -0700

    Adding DataAccess

---
+++ DataAccess/NVL9.DataAccess/Class1.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Class1.cs" company="SelectHealth.org">
+//     Copyright (c) 2019 SelectHealth.org.  All rights reserved.
+// </copyright>
+// <summary>
+//     Document created on 2019-01-23-4:22 PM
+// </summary>
+// <author>Andres Sosa (imail2)</author>
+
+namespace NVL9.DataAccess
+{
+    public class Class1
+    {
+    }
+}
